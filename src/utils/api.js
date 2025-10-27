3399524560c5509cac4de4e358ede8493bee64e8
simon <simondjv2016@gmail.com>
2025-10-26 21:22:26 -0500

    fix api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://project-fenix-backend-5063.onrender.com/';
+const API_URL = 'https://project-fenix-backend-5063.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
